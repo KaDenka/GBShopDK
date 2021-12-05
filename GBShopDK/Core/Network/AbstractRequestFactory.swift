af69fc99684bf8e8d66827e58564084389039917
Denis <recmanus@gmail.com>
2021-12-05 16:37:20 +0300

    hw2_Stage2
    Полностью создана структура файлов в соответствии с требованиями методички и материалов урока.

--- GBShopDK/Core/Network/AbstractRequestFactory.swift
+++ GBShopDK/Core/Network/AbstractRequestFactory.swift
@@ -4,5 +4,27 @@
 //
 //  Created by Denis Kazarin on 03.12.2021.
 //
-
 import Foundation
+import Alamofire
+
+protocol AbstractRequestFactory {
+    var errorParser: AbstractErrorParser { get }
+    var sessionManager: Session { get }
+    var queue: DispatchQueue { get }
+    
+    @discardableResult
+    func request<T: Decodable>(
+    request: URLRequestConvertible, completionHandler: @escaping (AFDataResponse<T>) -> Void) -> DataRequest
+    
+}
+
+extension AbstractRequestFactory {
+    
+    @discardableResult
+    public func request<T: Decodable>(
+    request: URLRequestConvertible, completionHandler: @escaping (AFDataResponse<T>) -> Void) -> DataRequest {
+        return sessionManager
+            .request(request)
+            .responseCodable(errorParser: errorParser, queue: queue, completionHandler: completionHandler)
+    }
+}
